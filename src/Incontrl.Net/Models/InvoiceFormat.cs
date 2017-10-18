ff90df4a8c6317ee55209bba75a5e8ee1c6b446f
Κωνσταντίνος Λευθέρης <c.leftheris@indice.gr>
2017-10-18 15:11:17 +0300

    minor changes

--- src/Incontrl.Net/Models/InvoiceFormat.cs
+++ src/Incontrl.Net/Models/InvoiceFormat.cs
@@ -1,7 +1,13 @@
 ﻿namespace Incontrl.Net.Models
 {
+    /// <summary>
+    /// Return type format when retrieving an invoice
+    /// </summary>
     public enum InvoiceFormat
     {
+        /// <summary>
+        /// <see cref="Pdf"/> return type format
+        /// </summary>
         Pdf
     }
 }
--- src/Incontrl.Net/Models/UpdateOrganisationRequest.cs
+++ src/Incontrl.Net/Models/UpdateOrganisationRequest.cs
@@ -68,10 +68,5 @@ namespace Incontrl.Net.Models
         /// additional/custom information for the organisation
         /// </summary>
         public dynamic CustomData { get; set; }
-
-        /// <summary>
-        /// This is for keeping track of the available methods for the company to recieve payment.
-        /// </summary>
-        public ICollection<PaymentOption> PaymentMethods { get; set; }
     }
 }
