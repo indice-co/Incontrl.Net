c7e10f64b0a247006cb7ff5c192602fee8c1abb5
Georgios Manoltzas <g.manoltzas@indice.gr>
2017-12-04 11:59:01 +0200

    Update models and delete unused models.

--- src/Incontrl.Sdk/Models/BulkLoadTransactionsRequest.cs
+++ src/Incontrl.Sdk/Models/BulkLoadTransactionsRequest.cs
@@ -3,19 +3,9 @@ using System.Collections.Generic;
 
 namespace Incontrl.Sdk.Models
 {
-    /// <summary>
-    /// 
-    /// </summary>
     public class BulkLoadTransactionsRequest
     {
-        /// <summary>
-        /// 
-        /// </summary>
         public Guid? BatchId { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public List<BankTransaction> Data { get; set; }
+        public List<Transaction> Data { get; set; }
     }
 }
--- src/Incontrl.Sdk/Models/PaymentOption.cs
+++ src/Incontrl.Sdk/Models/PaymentOption.cs
@@ -5,6 +5,7 @@ namespace Incontrl.Sdk.Models
     public class PaymentOption
     {
         public Guid? Id { get; set; }
+        public Guid? SubscriptionId { get; set; }
         public PaymentOptionType? Type { get; set; }
         public string Code { get; set; }
         public string Name { get; set; }
