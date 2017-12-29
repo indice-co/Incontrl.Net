60cae7248b167f750c1047c091d5bab1fc311eba
Georgios Manoltzas <g.manoltzas@indice.gr>
2017-12-29 22:00:58 +0200

    fix models

--- src/Incontrl.Sdk/Models/PricedLine.cs
+++ src/Incontrl.Sdk/Models/PricedLine.cs
@@ -8,6 +8,7 @@ namespace Incontrl.Sdk.Models
         public Guid Id { get; set; }
         public double Quantity { get; set; }
         public decimal UnitAmount { get; set; }
+        public decimal UnitAmountNet { get; set; }
         public double DiscountRate { get; set; }
         public List<Tax> Taxes { get; set; }
         public string TaxesDescription { get; set; }
