788d2d866dbacfe3467027dac84d3145da7e8dea
Κωνσταντίνος Λευθέρης <c.leftheris@indice.gr>
2017-12-30 15:42:27 +0200

    added Recipient name filter on document

--- src/Incontrl.Sdk/Models/DocumentListFilter.cs
+++ src/Incontrl.Sdk/Models/DocumentListFilter.cs
@@ -8,8 +8,9 @@ namespace Incontrl.Sdk.Models
         public string Number { get; set; }
         public DateTime? From { get; set; }
         public DateTime? To { get; set; }
-        public DocumentStatus? Status { get; set; }
+        public DocumentStatus[] Status { get; set; }
         public string RecipientCode { get; set; }
+        public string RecipientName { get; set; }
         public Guid? RecipientId { get; set; }
         public Guid[] TypeId { get; set; }
         public string PaymentCode { get; set; }
