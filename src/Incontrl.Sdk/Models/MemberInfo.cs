42528be89f172b5d18df77d15943905dcd0ab295
Georgios Manoltzas <g.manoltzas@indice.gr>
2018-01-18 11:37:12 +0200

    small fix on model

--- src/Incontrl.Sdk/Models/MemberInfo.cs
+++ src/Incontrl.Sdk/Models/MemberInfo.cs
@@ -2,6 +2,8 @@
 {
     public class MemberInfo
     {
+        public string Id { get; set; }
+        public MemberType Type { get; set; }
         public string Email { get; set; }
         public string DisplayName { get; set; }
         public string Url { get; set; }
