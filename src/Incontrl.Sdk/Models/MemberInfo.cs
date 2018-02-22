532bff5a5c0505b4e5d7658bfba59385a6b9d2f9
Georgios Manoltzas <g.manoltzas@indice.gr>
2018-02-22 15:41:00 +0200

    Update models and packages

--- src/Incontrl.Sdk/Models/MemberInfo.cs
+++ src/Incontrl.Sdk/Models/MemberInfo.cs
@@ -4,8 +4,46 @@
     {
         public string Id { get; set; }
         public MemberType Type { get; set; }
+        public MemberRoleType Role { get; set; }
         public string Email { get; set; }
         public string DisplayName { get; set; }
         public string Url { get; set; }
+    }
+
+    /// <summary>
+    /// The type of subscription member.
+    /// </summary>
+    public enum MemberType
+    {
+        /// <summary>
+        /// Application
+        /// </summary>
+        Application,
+
+        /// <summary>
+        /// User
+        /// </summary>
+        User
+    }
+
+    /// <summary>
+    /// The type of the role of the subscription member.
+    /// </summary>
+    public enum MemberRoleType
+    {
+        /// <summary>
+        /// Member
+        /// </summary>
+        Member = 0,
+
+        /// <summary>
+        /// Contributor
+        /// </summary>
+        Contributor = 1,
+
+        /// <summary>
+        /// Owner
+        /// </summary>
+        Owner = 2
     }
 }
