69b2f1d4edb0f7d8b3be8b0efe01c9627e15d1da
p.sorras <p.sorras@indice.gr>
2020-11-12 18:32:00 +0200

    Added new calls for Aade

--- src/Incontrl.Sdk/Services/DocumentApi.cs
+++ src/Incontrl.Sdk/Services/DocumentApi.cs
@@ -46,6 +46,10 @@ namespace Incontrl.Sdk.Services
 
         public Task<Document> UpdateAsync(UpdateDocumentRequest request, CancellationToken cancellationToken = default(CancellationToken)) =>
             _clientBase.PutAsync<UpdateDocumentRequest, Document>($"subscriptions/{SubscriptionId}/documents/{DocumentId}", request, cancellationToken);
+        public Task<MyDataResponse> SendToIAPR(CancellationToken cancellationToken = default(CancellationToken)) =>
+            _clientBase.GetAsync<MyDataResponse>($"subscriptions/{SubscriptionId}/documents/{DocumentId}/aade", cancellationToken);
+        public Task<Document> UpdateMarkAsync(UpdateDocumentAadeFieldsRequest request, CancellationToken cancellationToken = default(CancellationToken)) =>
+            _clientBase.PutAsync<UpdateDocumentAadeFieldsRequest, Document>($"subscriptions/{SubscriptionId}/documents/{DocumentId}/mark", request, cancellationToken);
 
         public IDocumentStatusApi Status() {
             var documentStatusApi = _documentStatusApi.Value;
