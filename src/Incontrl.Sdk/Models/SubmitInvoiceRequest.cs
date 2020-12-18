cccf7a826b3cb6f409873f62ac140d6b05d2cb4b
p.sorras <p.sorras@indice.gr>
2020-12-18 13:49:23 +0200

    Extended SubmitInvoiceRequest

--- src/Incontrl.Sdk/Models/SubmitInvoiceRequest.cs
+++ src/Incontrl.Sdk/Models/SubmitInvoiceRequest.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Incontrl.Sdk.Models
 {
     public class SubmitInvoiceRequest
     {
         public Guid[] RelatedDocumentIds { get; set; }
+        public IEnumerable<SubmitInvoicePaymentRequest> Payments { get; set; } = new List<SubmitInvoicePaymentRequest>();
+    }
+
+    public class SubmitInvoicePaymentRequest
+    {
+        public string Classification { get; set; }
+        public Money Amount { get; set; }
     }
 }
